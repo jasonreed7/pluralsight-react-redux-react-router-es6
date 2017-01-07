5a3eacd6d20e5f0759249f5684b13b6a2484f82a
Jason Reed <jreed0719@gmail.com>
2017-01-06 19:51:52 -0500

    Work up to but not including Async in Redux: Removing inline form

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
-import HomePage from './components/home/Homepage';
+import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
 
